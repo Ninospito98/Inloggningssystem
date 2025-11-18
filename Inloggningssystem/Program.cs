f99dd49af3f9bcc1af79c0c48108a8934ff805fa
Ninos Pito <Ninospito@hotmail.com>
2025-11-18 11:27:04 +0100

    Add project files.

---
+++ Inloggningssystem/Program.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel;
+
+
+namespace Inloggningssystem
+{
+    internal class Program
+    {
+        static bool running = true;
+
+        //Det går att använda lists så man kan skapa så många konto som möjligt.
+        //just nu man kan bara skapa 5 konto, går att ändra!
+        static string[] userName = new string[5];
+        static string[] passWord = new string[5];
+        static int userCount = 0;
+
+        static void Main(string[] args)
+        {
+
+            while (running)
+            {
+                Console.Clear();
+                Console.WriteLine("Välkommen till inloggningssidan!");
+                Console.WriteLine();
+                Console.WriteLine("Välj en åtgärd:");
+                Console.WriteLine("1. Sign in");
+                Console.WriteLine("2. Log in");
+                Console.WriteLine("3. Avsluta programmet");
+                Console.WriteLine($"{userName.Length}");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        SignIn();
+
+                        break;
+
+
+                    case "2":
+                        LogIn();
+                        break;
+
+                    case "3":
+                        running = false;
+                        Console.WriteLine("Tack, Vi ses!");
+                        Console.ReadKey();
+                        break;
+
+                    default:
+                        Console.WriteLine("Välja (1) (2) eller (3)");
+                        Console.ReadKey();
+                        break;
+                }
+
+            }
+
+        }
+
+        static void SignIn()
+        {
+            bool skapa = true;
+
+            while (skapa)
+            {
+                Console.Clear();
+                Console.Write("Ange ett användarnamn: ");
+                string newUserName = Console.ReadLine();
+                bool finns = false;
+                for (int i = 0; i < userCount; i++)
+                {
+                    if (newUserName == userName[i])
+                    {
+                        
+                        finns = true;
+                    }
+
+                }
+                if (finns)
+                {
+                    Console.WriteLine("Konto finns redan, try again");
+                    Console.ReadKey();
+                    continue;
+                }
+                Console.Write("Ange ett lösenord: ");
+                string newPassWord = Console.ReadLine();
+
+                userName[userCount] = newUserName;
+                passWord[userCount] = newPassWord;
+                userCount++;
+
+                Console.Clear();
+                Console.WriteLine("Du har skapat ett konto.");
+                Console.WriteLine();
+                Console.WriteLine($"Ditt användarnamn är: {newUserName}");
+                Console.WriteLine($"Ditt lösenord är: {newPassWord}");
+                Console.WriteLine();
+                Console.WriteLine("Gå tillbaka till huvudmenyn för att logga in.");
+                Console.ReadKey();
+                skapa = false;
+            }
+            
+
+        }
+
+        static void LogIn()
+        {
+               Console.Clear();
+                Console.Write("Användarnamn: ");
+                string typeUsername = Console.ReadLine();
+                Console.Write("Lösenord: ");
+                string typePassword = Console.ReadLine();
+
+                bool hittad = false;
+                
+                for (int i = 0; i < userCount; i++)
+                {
+                    
+                    if (typeUsername == userName[i] && typePassword == passWord[i])
+                    {
+                        Console.Clear();
+                        Console.WriteLine("you are logged in");
+                        Console.WriteLine($"Hello {userName[i]}");
+                        Console.ReadKey();
+                        hittad = true;                
+                        break;
+                    }
+                 }
+                if (!hittad)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Fel användarnamn och lösenord, Försök igen!");
+                    Console.ReadKey();
+
+                }
+
+        }
+    }
+}
